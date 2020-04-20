e2b21dc394ae4e62e71ea6f101a6eb64ba50dd24
Jin <jinruncai@gmail.com>
2020-04-20 14:36:19 -0700

    Add body-parser and add static routes

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const db = require('../database/');
 const tags = require('./routes/tags');
 
 const app = express();
+
+app.use(express.static(
+    path.resolve(__dirname, '..', 'public')
+));
+
+app.use(bodyParser.json());
 
 app.use('/tags', tags);
 
