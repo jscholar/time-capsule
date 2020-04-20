5ea5054132f482c817a2bab0d0d72dbe8cfaab77
Jin Cai <jinruncai@gmail.com>
2020-04-20 10:54:11 -0700

    Install redux

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
+import store from './store/store';
+
 import App from './components/App/App';
 
-const app = <App />;
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
 const containerElement = document.getElementById('app');
 
 ReactDOM.render(app, containerElement);
