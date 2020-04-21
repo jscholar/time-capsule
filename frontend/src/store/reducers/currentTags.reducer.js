590edf0b3d067592f3b05d84955f46425aa0cc28
Jin <jinruncai@gmail.com>
2020-04-20 21:34:04 -0700

    Forbid duplicate tags

--- frontend/src/store/reducers/currentTags.reducer.js
+++ frontend/src/store/reducers/currentTags.reducer.js
@@ -2,6 +2,9 @@ const currentTags = (state = [], action) => {
     const { type } = action;
     switch(type) {
         case 'SELECT_TAG':
+            if (state.includes(action.tagId)) {
+                return state;
+            }
             const newCurrentTags = state.slice();
             newCurrentTags.push(action.tagId);
             return newCurrentTags;
