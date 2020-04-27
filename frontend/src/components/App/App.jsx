035d9140ac9490bae2d01123738c2ced684fc98b
Jin <jinruncai@gmail.com>
2020-04-27 00:33:03 -0700

    Implement bootstrap column layout

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -7,12 +7,17 @@ import fetchEntries from '../../store/actions/fetchEntries.action';
 import TagList from '../Tag/TagList';
 import EntryList from '../Entry/EntryList';
 import Entry from '../Entry/Entry';
+import EntryForm from '../Entry/EntryForm';
 
 import './App.css';
 
 class App extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            entryState: 'view',
+        }
+        this.toggleCompose = this.toggleCompose.bind(this);
     }
 
     componentDidMount() {
@@ -21,14 +26,32 @@ class App extends Component {
         fetchTags();
         fetchEntries();
     }
+
+    toggleCompose() {
+        this.setState({
+            entryState: this.state.entryState === 'view' ? 'compose' : 'view'
+        });
+    }
     
-    render() {        
+    render() {
+        const { entryState } = this.state;
+        const entry = entryState === 'view' ? <Entry /> : <EntryForm />;
         return (
-            <div>
-                Time Capsule
-                <EntryList></EntryList>
-                <Entry></Entry>
-                <TagList></TagList>
+            <div className="container">
+                <div onClick={this.toggleCompose}>
+                    Compose
+                </div>
+                <div className="row">
+                    <div className="col-3">
+                        <EntryList />
+                    </div>
+                    <div className="col-6">
+                        {entry}
+                    </div>
+                    <div className="col-3">
+                        <TagList />
+                    </div>
+                </div>
             </div>
         )
     }
