4abe8385e6664609c866cc8da00dc1b24572a4f6
Jin <jinruncai@gmail.com>
2020-04-27 00:31:49 -0700

    Format sender time to human readable

--- frontend/src/components/Entry/EntryListItem.jsx
+++ frontend/src/components/Entry/EntryListItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import moment from 'moment';
 
 import { connect } from 'react-redux';
 import selectEntry from '../../store/actions/selectEntry.action';
 
 const EntryListItem = ({ entry, selectEntry }) => (
     <div onClick={() => selectEntry(entry)}>
-        {entry.senderTime}
+        {moment(entry.senderTime).format('LLL')}
     </div>
 )
 
